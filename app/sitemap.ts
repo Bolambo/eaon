348fe8b5e59ee00cdb0fdfa939c9740c5890263b
centman <gregmore4real@gmail.com>
2026-04-26 12:06:33 +0100

    Add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,32 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://eaonenergy.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://eaonenergy.com/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://eaonenergy.com/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://eaonenergy.com/capabilities",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://eaonenergy.com/hse-sustainability",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://eaonenergy.com/certifications",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://eaonenergy.com/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
